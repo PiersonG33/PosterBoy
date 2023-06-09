a21c534fc974a7a2594ed5435a9b47aaf7aac8ae
telescopeman <80982701+telescopeman@users.noreply.github.com>
2023-06-09 15:01:28 -0400

    Add color functionality
    Allow for easier changing of colors of post-it class.

--- posterboy/src/components/post-it.jsx
+++ posterboy/src/components/post-it.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Card, CardBody, CardFooter, Box } from '@chakra-ui/react'
 
+// The color of the post-it. This could become an argument of the class rather than a constant in the future,
+// for differently-colored post-its.
+const postItHue = "yellow";
+
+// These are helper functions to generate light and dark variations of the color.
+function lightColor({hue})
+{ return postItHue.concat(".100"); }
+
+function darkColor({hue})
+{ return postItHue.concat(".100"); }
 
 // "content" is the text inside the post-it
 function PostIt(props) {
     return(
       <Card 
-       bg="yellow.100" color="black"
+       bg={lightColor({postItHue})} color="black"
        w="500px" h="300px"
       >
         {/* Show content of the Post in the main section. */}
@@ -31,7 +41,7 @@ function PostIt(props) {
                 borderBottomColor="transparent"
                 borderBottomStyle="solid"
                 borderLeftWidth={50}
-                borderLeftColor="yellow.200"
+                borderLeftColor={darkColor({postItHue})}
                 borderLeftStyle="solid"
             />
             <Box
