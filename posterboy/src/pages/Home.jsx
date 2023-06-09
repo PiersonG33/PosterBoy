035dfbb964d8fd939ac36899296c85959ba00a08
telescopeman <80982701+telescopeman@users.noreply.github.com>
2023-06-09 15:37:15 -0400

    Add comments
    Add comments to clarify what the post it is there for

--- posterboy/src/pages/Home.jsx
+++ posterboy/src/pages/Home.jsx
@@ -5,11 +5,14 @@ import PostIt from './../components/post-it';
 function Home() {
   return(
     <HomeContainer>
+
+      {/* This post-it is here for debugging and messing around with the formatting. */}
       <PostIt 
         title   = "Welcome to PosterBoy!" 
         content = "Posterboy is the website of the future. All other websites are run by out-of-touch boomers. Join now, or be left behind! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." 
         author  = "PosterBoy Team"
       />
+      
     </HomeContainer>
   );
 }
