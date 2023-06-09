3f8c38d1607dbbda9f581106df8300a77a2864b1
Becky Chen <chenb9@rpi.edu>
2023-06-09 14:58:43 -0400

    chenb9- created the header

--- posterboy/src/components/Header.jsx
+++ posterboy/src/components/Header.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { FaFacebookSquare, FaLinkedin, FaGithubSquare, FaInstagram} from "react-icons/fa";
-import { ButtonGroup,  IconButton } from '@chakra-ui/react'
+import { FaHome, FaUserCircle, FaRegQuestionCircle, FaAngleDown} from "react-icons/fa";
+import { Button, ButtonGroup,  IconButton, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
 import LogoPic from '../assets/logo.svg';
 
 function Header() {
@@ -11,26 +10,24 @@ function Header() {
       <HeaderInnerContainer>
         <HeaderLeft>
           <Logo src={LogoPic} />
+          <Menu>
+            <MenuButton as={Button} color='#003F91'  rightIcon={<FaAngleDown/>}>
+              All Boards  
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Board 1</MenuItem>
+              <MenuItem>Board 2</MenuItem>
+              <MenuItem>Board 3</MenuItem>
+              <MenuItem>Board 4</MenuItem>
+              <MenuItem>Board 5</MenuItem>
+            </MenuList>
+          </Menu>
         </HeaderLeft>
-        <HeaderCenter>
-          <HeaderNavigationContainer>
-            <StyledLink to="/">Home</StyledLink>
-            <StyledLink to="/Board">Board</StyledLink>
-            <StyledLink to="/">Link_1</StyledLink>
-            <StyledLink to="/">Link_2</StyledLink>
-          </HeaderNavigationContainer>
-        </HeaderCenter>
         <HeaderRight>
         <ButtonGroup variant="ghost">
-            <IconButton
-              as="a"
-              href="/"
-              aria-label="Facebook"
-              icon={<FaFacebookSquare fontSize="1.25rem" />}
-            />
-            <IconButton as="a" href="/" aria-label="Instagram" icon={<FaInstagram fontSize="1.25rem" />} />
-            <IconButton as="a" href="https://github.com/PiersonG33/PosterBoy/tree/front-end/posterboy" aria-label="Github" icon={<FaGithubSquare fontSize="1.25rem" />} />
-            <IconButton as="a" href="/" aria-label="LinkedIn" icon={<FaLinkedin fontSize="1.25rem" />} />
+            <IconButton as="a" href="/" color='#FFCF00' aria-label="Home" icon={<FaHome fontSize="1.75rem" />} />
+            <IconButton as="a" href="/" color='#FFCF00' aria-label="Profile" icon={<FaUserCircle fontSize="1.75rem" />} />
+            <IconButton as="a" href="/" color='#FFCF00' aria-label="Help" icon={<FaRegQuestionCircle fontSize="1.75rem" />} />
           </ButtonGroup>
         </HeaderRight>
       </HeaderInnerContainer>
@@ -41,27 +38,22 @@ function Header() {
 
 const HeaderContainer = styled.nav`
   width: 100%;
-  background-color: #FDBA74;
+  background-color:#003F91;
   display: flex;
   flex-direction: column;
 `;
 
 const HeaderLeft = styled.div`
-  flex: 30%;
+  margin-left: 50px;
   display: flex;
-  justify-content: center;
-  
+  align-items: center;
+  width: 50%;
 `;
 
 const Logo = styled.img`
-  margin: 10px;
+  margin: 15px;
   height: auto;
-  max-width: 180px;
-`;
-
-const HeaderCenter = styled.div`
-  display: flex;
-  align-items: center;
+  max-width: 100px;
 `;
 
 const HeaderRight = styled.div`
@@ -70,6 +62,7 @@ const HeaderRight = styled.div`
   align-items: center;
   justify-content: end;
   margin-right: 50px;
+  width: 50%;
 `
 
 const HeaderInnerContainer = styled.div`
@@ -81,19 +74,5 @@ const HeaderInnerContainer = styled.div`
 const HeaderNavigationContainer = styled.div`
   display: flex;
 `; 
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  border-bottom: black solid 3px;
-  font-weight: bolder;
-  color: black;
-  font-size: large;
-  margin: 20px;
-  font-family: 'Work Sans', sans-serif;
-  &:hover{
-    color: white;
-    border-bottom: white solid 3px;
-  }
-`;
 
 export default Header;
