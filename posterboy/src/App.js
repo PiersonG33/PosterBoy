083cf2c7c324abf19d5fe3263bb13dc7c8726dcd
telescopeman <80982701+telescopeman@users.noreply.github.com>
2023-06-13 14:50:57 -0400

    Create preliminary "About"/"Help" page
    Creating a page that will contain helpful information, and linking it into the structure

--- posterboy/src/App.js
+++ posterboy/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import BoardPage from './pages/BoardPage';
+import AboutPage from './pages/AboutPage';
 import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Board" element={<BoardPage />} />
+          <Route path="/About" element={<AboutPage />} />
         </Routes>
       <Footer />
     </Router>
