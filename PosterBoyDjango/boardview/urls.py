0c24bb583c143a4c93e52ec6942c05a844311373
BobMcPhil <bobmcphil234@gmail.com>
2023-06-16 15:32:28 -0400

    Added the add_post method

--- PosterBoyDjango/boardview/urls.py
+++ PosterBoyDjango/boardview/urls.py
@@ -1,13 +1,5 @@
 from django.urls import path, include
-<<<<<<< Updated upstream
 from . import views
 urlpatterns=[
   path('',views.index, name="index")
-=======
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
->>>>>>> Stashed changes
 ]
