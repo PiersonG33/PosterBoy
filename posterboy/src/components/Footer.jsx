fcbc1c8763743352b79a087ac2efcc7ca892df74
telescopeman <80982701+telescopeman@users.noreply.github.com>
2023-06-16 13:39:10 -0400

    Remove buttons that will not be used

--- posterboy/src/components/Footer.jsx
+++ posterboy/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { FaFacebookSquare, FaLinkedin, FaGithubSquare, FaInstagram} from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
 import { ButtonGroup,  IconButton } from '@chakra-ui/react'
 import RCOSLogo from '../assets/rcos_logo.png';
 
@@ -24,14 +24,6 @@ function Footer() {
         <FooterRight>
           {/* This component displays social media icons */}
           <ButtonGroup variant="ghost" marginRight="1rem">
-            <IconButton
-              as="a"
-              color='#FFCF00' 
-              href="/"
-              aria-label="Facebook"
-              icon={<FaFacebookSquare fontSize="1.25rem" />}
-            />
-            <IconButton as="a" color='#FFCF00' href="/" aria-label="Instagram" icon={<FaInstagram fontSize="1.25rem" />} />
             <IconButton as="a" color='#FFCF00' href="https://github.com/PiersonG33/PosterBoy/tree/front-end/posterboy" aria-label="Github" icon={<FaGithubSquare fontSize="1.25rem" />} />
           </ButtonGroup>
           <img src={RCOSLogo} alt="RCOS Logo" style={{ height: "1.25rem", width: "1.25rem" }} />
