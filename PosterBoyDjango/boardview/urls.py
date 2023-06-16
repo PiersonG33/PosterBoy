b73c36be58121b52a7413eaa03d7e6a77b7ccab7
ThatBlueJay <61397831+ThatBlueJay@users.noreply.github.com>
2023-06-16 15:19:04 -0400

    Fixed strange conflict issue with urls.py

--- PosterBoyDjango/boardview/urls.py
+++ PosterBoyDjango/boardview/urls.py
@@ -1,16 +1,8 @@
 from django.urls import path, include
-<<<<<<< Updated upstream
 from . import views
 from api.views import note
 from boardview.views import index
 
 urlpatterns=[
   path('',note, name="boardview"),
-=======
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
->>>>>>> Stashed changes
 ]
