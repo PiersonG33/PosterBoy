56d36b1b7d6933328afb7babba6fad7a8e4e6517
BobMcPhil <bobmcphil234@gmail.com>
2023-06-20 15:17:52 -0400

    Updated boardview models and view
    made lower_score, get_user_actions. Made add_post and lower_score update the UserActions table

--- PosterBoyDjango/boardview/models.py
+++ PosterBoyDjango/boardview/models.py
@@ -15,4 +15,45 @@ class Post(models.Model):
 
     def __str__(self):
         return self.message
+    
 
+class Board(models.Model):
+    boardid = models.IntegerField()
+    topic_name = models.CharField(max_length=50)
+    actions = models.IntegerField()
+    reset = models.DurationField()
+
+    def __str__(self):
+        return self.topic_name
+    
+class UserActions(models.Model):
+    actionid = models.IntegerField()
+    uid = models.IntegerField()
+    pid = models.IntegerField()
+    action = models.CharField(max_length=10)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.uid) + self.action
+    
+class UserStatus(models.Model):
+    uid = models.IntegerField()
+    boardid = models.IntegerField()
+    status = models.CharField(max_length=10)
+
+    def __str__(self):
+        return str(self.uid) + self.status
+    
+class PostArchive(models.Model):
+    message = models.CharField(max_length=200)
+    uid = models.IntegerField()
+    pid = models.IntegerField()
+    boardid = models.IntegerField()
+    color = models.IntegerField()
+    date = models.DateTimeField()
+    score = models.IntegerField(default=1) # posts start with a score of 1
+    xCoord = models.IntegerField()
+    yCoord = models.IntegerField()
+
+    def __str__(self):
+        return self.message
