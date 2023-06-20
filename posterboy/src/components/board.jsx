91e9ab3c24a170576302ec2c6f57cb87c155e15a
Becky Chen <chenb9@rpi.edu>
2023-06-20 13:59:55 -0400

    chenb9-node-modules

--- posterboy/src/components/board.jsx
+++ posterboy/src/components/board.jsx
@@ -1,17 +1,22 @@
 import { Space } from 'react-zoomable-ui';
 
 import Board_Pic from "../assets/board_stock_pic.png";
+import Counter from "boardCounter.jsx";
 
 const imageWidth = 2621;
 const imageHeight = 1805;
 
 function Board() {
   return (
-    <div style={{ width: '100vw', height: '90vw', position: "relative" }}>
-      <Space style={{ backgroundColor: '#FFCF0030' }} >
-        <img src={Board_Pic} width={imageWidth} height={imageHeight} alt="The Amazing Cork Board" />
-      </Space>
-    </div>
+    <BoardContainer>
+      <Counter/>
+      <div style={{ width: '100vw', height: '90vw', position: "relative" }}>
+        <Space style={{ backgroundColor: '#FFCF0030' }} >
+          <img src={Board_Pic} width={imageWidth} height={imageHeight} alt="The Amazing Cork Board" />
+        </Space>
+      </div>
+    </BoardContainer>
+
   )
 }
 
