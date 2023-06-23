a1801c01bb5ca991d198663055ce3be9ece66a6e
thezw27 <134734870+thezw27@users.noreply.github.com>
2023-06-23 15:33:55 -0400

    Update fill.py

--- PosterBoyDjango/fill.py
+++ PosterBoyDjango/fill.py
@@ -64,7 +64,7 @@ for b in range(3):
         dt = datetime.now(timezone.utc)
         x = random.randint(0,2300)
         y = random.randint(0,1800)
-        cursor.execute("INSERT INTO posts VALUES(" + str(user_id[0]) + ", DEFAULT, " + str(board_id[0]) + ", \'" + generateWord(200) + "\', " + str(0) + ", \'" + str(dt) + "\', " + str(0) + ", (" + str(x) + ", " + str(y) + "), " + str(random.randint(0, 10)) + ");")
+        cursor.execute("INSERT INTO posts VALUES(" + str(user_id[0]) + ", DEFAULT, " + str(board_id[0]) + ", \'" + generateWord(200) + "\', " + str(0) + ", \'" + str(dt) + "\', " + str(0) + ", " + str(x) + ", " + str(y) + ", " + str(random.randint(0, 10)) + ");")
 connection.commit()
         
 # Status - 10
