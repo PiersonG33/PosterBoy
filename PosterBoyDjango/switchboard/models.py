85cce55543b208c5d1072569565635dd299424bc
ThatBlueJay <61397831+ThatBlueJay@users.noreply.github.com>
2023-06-23 15:31:23 -0400

    Made some necessary changes to Model naming for boardswitches

--- PosterBoyDjango/switchboard/models.py
+++ PosterBoyDjango/switchboard/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
     
 
-class Board(models.Model):
+class Boards(models.Model):
     id = models.IntegerField()
     topic_name = models.TextField(max_length=50)
     actions = models.IntegerField()
@@ -13,10 +13,10 @@ class Board(models.Model):
     def __str__(self):
         return self.topic_name
 
-class Status(models.Model):
+class UserStatus(models.Model):
     userid = models.models.ForeignKey(User, on_delete=models.CASCADE)
     boardid = models.IntegerField()
     role = models.TextField(max_length=10)
 
     def __str__(self):
-        return f"{self.userid.username} - {self.boardid.topic_name} - {self.role}"
+        return "{self.userid.username} - {self.boardid.topic_name} - {self.role}"
