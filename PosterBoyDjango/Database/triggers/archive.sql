bf0080c53e50d8553f4e5dae34b3403f30af62dd
Adsalor <adsalor@gmail.com>
2023-06-30 15:38:09 -0400

    database triggers and constraint modifications
    also bugfix for a 0.0009% chance fill.py bug

--- PosterBoyDjango/Database/triggers/archive.sql
+++ PosterBoyDjango/Database/triggers/archive.sql
@@ -4,10 +4,9 @@ DROP FUNCTION IF EXISTS archiver_trgf;
 CREATE FUNCTION archiver_trgf() RETURNS TRIGGER as $$
 BEGIN
     IF NEW.score <= 0 THEN 
-        INSERT INTO postarchive VALUES(DEFAULT, NEW.user_id, NEW.board_id, NEW.message, NEW.message_type, NOW);
-        NEW := NULL;
+        INSERT INTO postarchive VALUES(DEFAULT, NEW.userid, NEW.boardid, NEW.message, NEW.message_type, now());
+        DELETE FROM posts WHERE id = NEW.id;
     END IF;
-    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
