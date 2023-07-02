41b21de4b0238a028489bbe02f90c212f59f1176
Adsalor <adsalor@gmail.com>
2023-07-02 16:35:18 -0400

    third trigger works now

--- PosterBoyDjango/Database/del.sql
+++ PosterBoyDjango/Database/del.sql
@@ -1,6 +1,7 @@
 DELETE FROM userstatus * CASCADE;
 DELETE FROM useractions * CASCADE;
 DELETE FROM posts * CASCADE;
+DELETE FROM postarchive * CASCADE;
 DELETE FROM boards * CASCADE;
 DELETE FROM auth_user * CASCADE;
 
--- PosterBoyDjango/Database/triggers/archive.sql
+++ PosterBoyDjango/Database/triggers/archive.sql
@@ -5,8 +5,11 @@ CREATE FUNCTION archiver_trgf() RETURNS TRIGGER as $$
 BEGIN
     IF NEW.score <= 0 THEN 
         INSERT INTO postarchive VALUES(DEFAULT, NEW.userid, NEW.boardid, NEW.message, NEW.message_type, now());
+        UPDATE useractions SET postid = NULL WHERE postid = NEW.id;
         DELETE FROM posts WHERE id = NEW.id;
+        RETURN NULL;
     END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
